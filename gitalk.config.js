b60fd11f7aa45ba3395992fb8644c8b47bc6b0e8
Veng <1216116203@qq.com>
2019-09-12 10:52:06 +0800

    gitalk配置文件

---
+++ gitalk.config.js
@@ -0,0 +1,10 @@
+const gitalk = new Gitalk({
+    "clientID": "d3491cdc4dc44e40ddff",
+    "clientSecret": "caf877b323fd49de292dcb4262b35004ec328202",
+    "repo": "veng0923.github.io",
+    "owner": "veng0923",
+    "admin": ["veng0923"],
+    "id": window.location.pathname,
+    "distractionFreeMode": false
+  });
+  gitalk.render("gitalk-container");
