bb23b6ff149dfc73dd06fad4b02f57ca55a5d429
Veng <1216116203@qq.com>
2019-09-12 13:46:40 +0800

    评论修正

--- gitalk.config.js
+++ gitalk.config.js
@@ -1,10 +1,12 @@
-const gitalk = new Gitalk({
-  "clientID": "d3491cdc4dc44e40ddff",
-  "clientSecret": "caf877b323fd49de292dcb4262b35004ec328202",
-  "repo": "HTMLDemo",
-  "owner": "veng0923",
-  "admin": ["veng0923"],
-  "id": window.location.pathname,
-  "distractionFreeMode": false
-});
-gitalk.render("gitalk-container");
+window.onload=function(){
+  const gitalk = new Gitalk({
+    "clientID": "d3491cdc4dc44e40ddff",
+    "clientSecret": "caf877b323fd49de292dcb4262b35004ec328202",
+    "repo": "HTMLDemo",
+    "owner": "veng0923",
+    "admin": ["veng0923"],
+    "id": window.location.pathname,
+    "distractionFreeMode": false
+  });
+  gitalk.render("gitalk-container");
+};
